b1165c48fd55a04370be50ee02cb293369717d5f
Thomas Jepp <thomas.jepp@propeller.co.uk>
2017-04-12 16:14:29 +0100

    Handle initialising a markitup textarea properly when new CMS sections are added.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
-	$('textarea.markitup').markItUp(myTextileSettings);
+var markitupInit = {
+	init: function() {
+		$('.markitup:not(.markItUpEditor)').markItUp(myTextileSettings);
 
-	$('.markItUpButton1 a').html('<i class="icon-link"></i>');
-	$('.markItUpButton2 a').html('<i class="icon-italic"></i>');
-	$('.markItUpButton3 a').html('<i class="icon-bold"></i>');
-	$('.markItUpButton4 a').html('<i class="icon-strikethrough"></i>');
+		$('.markItUpButton1 a').html('<i class="icon-link"></i>');
+		$('.markItUpButton2 a').html('<i class="icon-italic"></i>');
+		$('.markItUpButton3 a').html('<i class="icon-bold"></i>');
+		$('.markItUpButton4 a').html('<i class="icon-strikethrough"></i>');
+	}
+}
+$(document).ready(function() {
+	markitupInit.init();
+});
+
+$('#sections-sortable').on('new-section.pagemanager', function() {
+	markitupInit.init();
 });
